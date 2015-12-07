3f82042a6e735529a61213041728dc473a706108
Carlos <programador_carlos@hotmail.com>
2015-12-07 15:31:01 -0600

    Se agrego la plantilla para el administrador del sistema

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,7 @@
 <?php
 
+// Direccion de la pagina web
 Route::get('/', 'InicioController@index');
+
+// Direccion del sistema
+Route::get('/sistema', 'SistemaController@index');
--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -1,4 +1,4 @@
-@extends('templates/plantilla')
+@extends('templates/pagina')
 
 @section('contenido')
     <header id="header">
@@ -11,7 +11,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html"><img src="{{ asset('assets/plantilla/images/logo.png') }}" alt="logo"></a>
+                    <a class="navbar-brand" href="index.html"><img src="{{ asset('assets/pagina/images/logo.png') }}" alt="logo"></a>
                 </div>
 
                 <div class="collapse navbar-collapse navbar-right">
@@ -32,7 +32,7 @@
 
     <section id="main-slider">
         <div class="owl-carousel">
-            <div class="item" style="background-image: url('{{ public_path() }}/images/slider/bg1.jpg');">
+            <div class="item" style="background-image: url({{ asset('assets/pagina/images/slider/bg1.jpg')  }});">
                 <div class="slider-inner">
                     <div class="container">
                         <div class="row">
@@ -45,9 +45,10 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div><!--/.item-->
-            <div class="item" style="background-image: url(images/slider/bg2.jpg);">
+            <div class="item" style="background-image: url({{ asset('assets/pagina/images/slider/bg2.jpg') }});">
                 <div class="slider-inner">
                     <div class="container">
                         <div class="row">
@@ -88,7 +89,7 @@
             </div>
             <div class="row">
                 <div class="col-sm-6 wow fadeInLeft">
-                    <img class="img-responsive" src="{{ asset('assets/plantilla/images/main-feature.png') }}" alt="">
+                    <img class="img-responsive" src="{{ asset('assets/pagina/images/main-feature.png') }}" alt="">
                 </div>
                 <div class="col-sm-6">
                     <div class="media service-box wow fadeInRight">
@@ -251,88 +252,88 @@
             <div class="portfolio-items">
                 <div class="portfolio-item creative">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/01.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/01.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 1</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item corporate portfolio">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/02.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/02.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 2</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item creative">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/03.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/03.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 3</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item corporate">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/04.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/04.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 4</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item creative portfolio">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/05.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/05.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 5</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item corporate">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/06.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/06.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 5</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item creative portfolio">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/07.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/07.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 7</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
 
                 <div class="portfolio-item corporate">
                     <div class="portfolio-item-inner">
-                        <img class="img-responsive" src="{{ asset('assets/plantilla/images/portfolio/08.jpg') }}" alt="">
+                        <img class="img-responsive" src="{{ asset('assets/pagina/images/portfolio/08.jpg') }}" alt="">
                         <div class="portfolio-info">
                             <h3>Portfolio Item 8</h3>
                             Lorem Ipsum Dolor Sit
-                            <a class="preview" href="{{ asset('assets/plantilla/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
+                            <a class="preview" href="{{ asset('assets/pagina/images/portfolio/full.jpg') }}" rel="prettyPhoto"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                 </div><!--/.portfolio-item-->
@@ -463,7 +464,7 @@
                 <div class="col-sm-6 col-md-3">
                     <div class="team-member wow fadeInUp" data-wow-duration="400ms" data-wow-delay="0ms">
                         <div class="team-img">
-                            <img class="img-responsive" src="{{ asset('assets/plantilla/images/team/01.jpg') }}" alt="">
+                            <img class="img-responsive" src="{{ asset('assets/pagina/images/team/01.jpg') }}" alt="">
                         </div>
                         <div class="team-info">
                             <h3>Bin Burhan</h3>
@@ -481,7 +482,7 @@
                 <div class="col-sm-6 col-md-3">
                     <div class="team-member wow fadeInUp" data-wow-duration="400ms" data-wow-delay="100ms">
                         <div class="team-img">
-                            <img class="img-responsive" src="{{ asset('assets/plantilla/images/team/02.jpg') }}" alt="">
+                            <img class="img-responsive" src="{{ asset('assets/pagina/images/team/02.jpg') }}" alt="">
                         </div>
                         <div class="team-info">
                             <h3>Jane Man</h3>
@@ -499,7 +500,7 @@
                 <div class="col-sm-6 col-md-3">
                     <div class="team-member wow fadeInUp" data-wow-duration="400ms" data-wow-delay="200ms">
                         <div class="team-img">
-                            <img class="img-responsive" src="{{ asset('assets/plantilla/images/team/03.jpg') }}" alt="">
+                            <img class="img-responsive" src="{{ asset('assets/pagina/images/team/03.jpg') }}" alt="">
                         </div>
                         <div class="team-info">
                             <h3>Pahlwan</h3>
@@ -517,7 +518,7 @@
                 <div class="col-sm-6 col-md-3">
                     <div class="team-member wow fadeInUp" data-wow-duration="400ms" data-wow-delay="300ms">
                         <div class="team-img">
-                            <img class="img-responsive" src="{{ asset('assets/plantilla/images/team/04.jpg') }}" alt="">
+                            <img class="img-responsive" src="{{ asset('assets/pagina/images/team/04.jpg') }}" alt="">
                         </div>
                         <div class="team-info">
                             <h3>Nasir uddin</h3>
@@ -812,13 +813,13 @@
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" role="listbox">
                             <div class="item active">
-                                <p><img class="img-circle img-thumbnail" src="{{ asset('assets/plantilla/images/testimonial/01.jpg') }}" alt=""></p>
+                                <p><img class="img-circle img-thumbnail" src="{{ asset('assets/pagina/images/testimonial/01.jpg') }}" alt=""></p>
                                 <h4>Louise S. Morgan</h4>
                                 <small>Treatment, storage, and disposal (TSD) worker</small>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam</p>
                             </div>
                             <div class="item">
-                                <p><img class="img-circle img-thumbnail" src="{{ asset('assets/plantilla/images/testimonial/01.jpg') }}" alt=""></p>
+                                <p><img class="img-circle img-thumbnail" src="{{ asset('assets/pagina/images/testimonial/01.jpg') }}" alt=""></p>
                                 <h4>Louise S. Morgan</h4>
                                 <small>Treatment, storage, and disposal (TSD) worker</small>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam</p>
@@ -914,15 +915,15 @@
         </div>
     </footer><!--/#footer-->
 
-    <script src="{{ asset('assets/plantilla/js/jquery.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/jquery.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/bootstrap.min.js') }}"></script>
     <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-    <script src="{{ asset('assets/plantilla/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/mousescroll.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/smoothscroll.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/jquery.prettyPhoto.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/jquery.isotope.min.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/jquery.inview.min.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/wow.min.js') }}"></script>
-    <script src="{{ asset('assets/plantilla/js/main.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/mousescroll.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/smoothscroll.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/jquery.prettyPhoto.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/jquery.isotope.min.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/jquery.inview.min.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/pagina/js/main.js') }}"></script>
 @stop
